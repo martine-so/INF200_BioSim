3de287e065d7ae3c3401af9f27027ff940469ea1
Martine Olsen <martine.sorgard.olsen@nmbu.no>
2022-01-04 10:09:37 +0100

    Moved functions back to Herbivore class

--- Herbivores.py
+++ Herbivores.py
@@ -1,3 +1,5 @@
+import math
+import random
 
 class Herbivore:
 
@@ -9,13 +11,50 @@ class Herbivore:
         self.alive = True
 
 
-    def update_weight(self, F, beta):
-        self.w += beta * F
+    def update_weight(self, f, beta):
+        self.w += beta * f
 
 
+    def calculate_fitness(self, phi_age, phi_weight, a_half, w_half):
+        if self.w <= 0:
+            self.fitness = 0
+
+        else:
+            self.fitness = (1/(1 + math.exp(phi_age * (self.a - a_half)))) * \
+                           (1/(1 + math.exp(-phi_weight * (self.w - w_half))))
+
+        return self.fitness
 
 
+    def breeding(self, zeta, w_birth, sigma_birth, xi, gamma, N):
+        if self.w < zeta * (w_birth + sigma_birth):
+            probability = 0
+        elif self.w < xi * w_birth:  # xi * babys vekt. Vet ikke helt hva babys vekt er?
+            probability = 0
+        else:
+            probability = min(1, gamma * self.fitness * (N - 1))
+
+        if random.random() < probability:
+            baby = True
+        else:
+            baby = False
+
+        return baby
 
 
+    def update_a(self):
+        self.a += 1
 
+
+    def loose_weight(self, eta):
+        self.w -= eta * self.w
+
+
+    def death(self, omega):
+        if self.w == 0:
+            self.alive = False
+        else:
+            probability = omega * (1 - self.fitness)
+            if random.random() < probability:
+                self.alive = False
 
