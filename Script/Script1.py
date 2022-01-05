7c10e55cd9e53a562a79037ce5f9f4db67368bcd
Karoline Kviserud <karoline.kviserud@nmbu.no>
2022-01-05 17:12:06 +0100

    Code recognizes placement of animals on geogr and type of land

--- Script/Script1.py
+++ Script/Script1.py
@@ -10,8 +10,6 @@ geogr = """\
            WWW"""
 geogr = textwrap.dedent(geogr)
 
-# print(geogr)
-
 ini_herbs = [{'loc': (2, 2),
               'pop': [{'species': 'Herbivore',
                        'age': 5,
@@ -19,35 +17,26 @@ ini_herbs = [{'loc': (2, 2),
                       for _ in range(50)]}]
 
 coordinates = [i['loc'] for i in ini_herbs]
-# print(coordinates)
 
-"""Finner ut at vi står i Lowland:"""
-f_max = 800
-F = 10
+for i in coordinates:
+    x, y = i
+location = geogr.split()[x-1][y-1]
+landtypes = {'L': Lowland}
+if location in landtypes:
+    landtype = landtypes[location]
+
 years = 50
-beta = 0.9
-phi_age = 0.6
-phi_weight = 0.1
-a_half = 40
-w_half = 10
-zeta = 3.5
-w_birth = 8
-sigma_birth = 1.5
-xi = 1.2
-gamma = 0.2
-eta = 0.05
-omega = 0.4
-
 for i in ini_herbs:
     animals = [Herbivore(j['age'], j['weight']) for j in i['pop']]
 
 for year in range(years):
-    lowlandfield = Lowland(animals)
-    lowlandfield.eating()
-    lowlandfield.breeding()
-    print(len(animals))
-    lowlandfield.aging()
-    lowlandfield.loose_weight()
-    lowlandfield.dying()
-    animals = lowlandfield.animals
+    field = landtypes[location](animals)
+    field.eating()
+    field.breeding()
+    # print(len(animals))
+    field.aging()
+    field.loose_weight()
+    field.dying()
+    animals = field.animals  # Får samme tall selv om denne kommenteres ut. Kan dette være kilden til feil tall??
     print(len(animals))  # Alle føder 2. året. WHYYYYYYY?!?!?!?!
+
