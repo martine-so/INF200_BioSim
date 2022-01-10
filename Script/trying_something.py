c80129f08c9da710c0e455990b6a26c37f2ad579
Karoline Kviserud <karoline.kviserud@nmbu.no>
2022-01-10 11:49:54 +0100

    Added a dictionary only including coordinates of land

--- Script/trying_something.py
+++ Script/trying_something.py
@@ -3,15 +3,19 @@ import Lowland
 
 geogr = """\
            WWW
-           WLW
+           WLD
            WWW"""
 geogr = textwrap.dedent(geogr)
 
 newGeogr = geogr.split()
 
 geogrDict = {}
+emptyGeogrDict = {}
 for i in range(len(newGeogr)):
     for j in range(len(newGeogr[0])):
         geogrDict['({0}, {1})'.format(i+1, j+1)] = newGeogr[i][j]
+        if newGeogr[i][j] != 'W':
+            emptyGeogrDict['({0}, {1})'.format(i+1, j+1)] = []
 print(geogrDict)
+print(emptyGeogrDict)
 
