eacbb625304eec7d19ffcc40fd1f37f27b0e73d8
Karoline Kviserud <karoline.kviserud@nmbu.no>
2022-01-10 14:19:45 +0100

    Corrected code in herbivores_class.py

--- Script/herbivores_class.py
+++ Script/herbivores_class.py
@@ -19,10 +19,11 @@ class Herbivore:
     gamma = 0.2
     eta = 0.05
     omega = 0.4
+    mu = 0.25
 
     default_params = {'F': F, 'beta': beta, 'phi_age': phi_age, 'phi_weight': phi_weight, 'a_half': a_half,
                       'w_half': w_half, 'zeta': zeta, 'w_birth': w_birth, 'sigma_birth': sigma_birth,
-                      'xi': xi, 'gamma': gamma, 'eta': eta, 'omega': omega}
+                      'xi': xi, 'gamma': gamma, 'eta': eta, 'omega': omega, 'mu': mu}
 
     @classmethod
     def set_params(cls, new_params):
@@ -33,24 +34,18 @@ class Herbivore:
             if key not in ('beta', 'phi_age', 'phi_weight', 'a_half', 'w_half', 'zeta', 'w_birth',
                            'sigma_birth', 'xi', 'gamma', 'eta', 'omega'):
                 raise KeyError('Invalid parameter name: ' + key)
+
             if not 0 <= new_params[key]:
                 raise ValueError('All parameter values must be positive')
+
+            if key == 'eta':
+                if not new_params['eta'] <= 1:
+                    raise ValueError('eta must be in [0, 1].')
+
+            if key == 'DeltaPhiMax':
+                if not 0 < new_params['DeltaPhiMax']:
+                    raise ValueError('DeltaPhiMax must be higher than 0')
             cls.key = new_params[key]
-
-        # for key in new_params:
-        #     if not 0 <= new_params[key]:
-        #         raise ValueError('All parameter values must be positive')
-        #     cls.key = new_params[key]
-
-        if 'eta' in new_params:
-            if not new_params['eta'] <= 1:
-                raise ValueError('eta must be in [0, 1].')
-            cls.eta = new_params['eta']
-
-        if 'DeltaPhiMax' in new_params:
-            if not 0 < new_params['DeltaPhiMax']:
-                raise ValueError('DeltaPhiMax must be higher than 0')
-            cls.DeltaPhiMax = new_params['DeltaPhiMax']
 
     @classmethod
     def get_params(cls):
@@ -58,8 +53,7 @@ class Herbivore:
         return {'F': cls.F, 'beta': cls.beta, 'phi_age': cls.phi_age, 'phi_weight': cls.phi_weight,
                 'a_half': cls.a_half, 'w_half': cls.w_half, 'zeta': cls.zeta, 'w_birth': cls.w_birth,
                 'sigma_birth': cls.sigma_birth, 'xi': cls.xi, 'gamma': cls.gamma, 'eta': cls.eta,
-                'omega': cls.omega}
-        # mu skal også inn her når de beveger seg
+                'omega': cls.omega, 'mu': cls.mu}
 
     def __init__(self, a=0, w=None):
         self.a = a
