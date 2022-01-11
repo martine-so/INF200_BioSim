dd2e1eedb3e7c4115bca26ea4398e250efd740cc
Martine Olsen <martine.sorgard.olsen@nmbu.no>
2022-01-11 16:38:55 +0100

    started adding doc-strings to tests

--- tests/test_Herbivore.py
+++ tests/test_Herbivore.py
@@ -3,70 +3,120 @@ from carnivores_class import Carnivore
 
 
 def test_init_herb_a_default():
+    """
+    Tests that a new herbivore gets default values if nothing else is given.
+    Default value for herbivore age is zero. Since newborns are zero years.
+    """
     herb = Herbivore()
     assert herb.a == 0
 
 
 def test_init_herb_a_defined():
+    """
+    Tests that a new herbivore gets given age value. Here age is set to 5
+    """
     herb = Herbivore(a=5, w=20)
     assert herb.a == 5
 
 
 def test_init_herb_w_default():  # Må se på dette
+    """
+    Tests that a new herbivore gets default values if nothing else is given.
+    """
     pass
 
 
 def test_init_herb_w_defined():
+    """
+    Tests that a new herbivore gets given weight value. Here weight is set to 20.
+    """
     herb = Herbivore(a=0, w=20)
     assert herb.w == 20
 
 
 def test_init_herb_fitness_default():
+    """
+    Tests that a new herbivore gets default values if nothing else is given.
+    Default value for herbivore fitness is zero.
+    """
     herb = Herbivore()
     assert herb.fitness == 0
 
 
 def test_init_herb_dead_default():
+    """
+    Tests that a new herbivore gets default values if nothing else is given.
+    Herbivore default value for dead is False.
+    """
     herb = Herbivore()
     assert herb.dead is False
 
 
 def test_init_carn_a_default():
+    """
+    Tests that a new carnivore gets default values if nothing else is given.
+    Default value for carnivores age is zero. That makes newborns zero years old.
+    """
     carn = Carnivore()
     assert carn.a == 0
 
 
 def test_init_carn_a_defined():
+    """
+    Tests that a new carnivore gets given age. Here age is set to 5 years.
+    """
     carn = Carnivore(a=5, w=20)
     assert carn.a == 5
 
 
 def test_init_carn_w_default():  # må se på dette
+    """
+    Tests that a new carnivore gets default values if nothing else is given.
+    """
     pass
 
 
 def test_init_carn_w_defined():
+    """
+    Tests that a new carnivore gets given weight. Here weight is set to 20.
+    """
     carn = Carnivore(a=0, w=20)
     assert carn.w == 20
 
 
 def test_init_carn_fitness_default():
+    """
+    Tests that a new carnivore gets default values if nothing else is given.
+    Default value for fitness is zero for carnivores.
+    """
     carn = Carnivore()
     assert carn.fitness == 0
 
 
 def test_init_carn_dead_default():
+    """
+    Tests that a new carnivore gets default values if nothing else is given.
+    Default value for dead is False for carnivore.
+    """
     carn = Carnivore()
     assert carn.dead is False
 
 
 def test_update_weight_herb():
+    """
+    Tests the function that makes Herbivores gain weight as they eat.
+    If they eat f=10 times beta=0.9 as default. The weight should go up by 9.
+    """
     herb = Herbivore(a=0, w=20)
     herb.update_weight(f=10)
     assert herb.w == 29
 
 
 def test_update_weight_carn():
+    """
+    Tests the function that makes carnivores gain weight as they eat.
+    If they eat f=50 times beta=0.75 as default. The weight should go up by 37.5.
+    """
     carn = Carnivore(a=0, w=20)
     carn.update_weight(f=50)
     assert carn.w == 57.5
