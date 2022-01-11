54a8da4e548b575ca3d87f6706cf5efb8c3e6084
Martine Olsen <martine.sorgard.olsen@nmbu.no>
2022-01-11 13:05:52 +0100

    made some tests. One fails, look at it later, might be my calculations that are wrong

--- tests/test_Herbivore.py
+++ tests/test_Herbivore.py
@@ -1,21 +1,40 @@
 from herbivores_class import Herbivore
+from carnivores_class import Carnivore
+from animals_class import Animals
 
 
-def test_update_weight():
-     a = 0
-     w = 20
-     f = 10
-     h = Herbivore(a, w)
-     h.update_weight(f)
-     assert h.w == 29
+def test_update_weight_herb():
+     herb = Herbivore(a=0, w=20)
+     herb.update_weight(f=10)
+     assert herb.w == 29
 
-def test_calculate_fitness():
-    h= Herbivore()
-    if h.w < 0:
-        assert h.fitness == 0
+
+def test_update_weight_carn():
+    carn = Carnivore(a=0, w=20)
+    carn.update_weight(f=50)
+    assert carn.w == 57.5
+
+
+def test_calculate_fitness_herb_weight_under_zero():
+    herb= Herbivore()
+    if herb.w < 0:
+        assert herb.fitness == 0
+
+
+def test_calculate_fitness_herb_weight_positive():
+    herb = Herbivore(a=40, w=10)
+    assert herb.fitness == 0  # skal vist være null?? Er det mattekunskaper det står i? 0.25??
+
+
+def test_calculate_fitness_carn():
+    carn= Carnivore()
+    if carn.w < 0:
+        assert carn.fitness == 0
+
 
 def test_breeding():
     pass
+
 
 def test_update_a_and_w():
     h = Herbivore()
