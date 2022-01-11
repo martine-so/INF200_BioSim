33d923a6728f6d54c4178758d9ff361c40814339
Martine Olsen <martine.sorgard.olsen@nmbu.no>
2022-01-11 20:49:48 +0100

    attempted a test. it does not work tho

--- tests/test_Lowland.py
+++ tests/test_Lowland.py
@@ -1,10 +1,14 @@
-# from lowland_class import Lowland
+from lowland_class import Lowland
 from landscape_class import Landscape
 from herbivores_class import Herbivore
 from carnivores_class import Carnivore
 
 def test_eating_herbivores():
-    pass
+    herb = Herbivore(a=5, w=20)
+    carn = Carnivore(a=5, w=20)
+    field = Lowland()
+    field.eating_herbivores()
+    assert herb.w == 29
 
 def test_prob_carn_eating():
     pass
